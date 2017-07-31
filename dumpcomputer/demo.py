28545b2db11448038dd36c5bb693535f292b3bdb
root <root@vm33-ysj.(none)>
2017-07-31 15:54:54 +0800

    123

--- dumpcomputer/demo.py
+++ dumpcomputer/demo.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python
 
-# Copyright (C) 2003-2007  Robey Pointer <robeypointer@gmail.com>
-#
-# This file is part of paramiko.
-#
 # Paramiko is free software; you can redistribute it and/or modify it under the
 # terms of the GNU Lesser General Public License as published by the Free
 # Software Foundation; either version 2.1 of the License, or (at your option)
